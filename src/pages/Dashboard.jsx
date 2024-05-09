31d14f52cf97fd1b49c0d0e56a3dc0ddebaa36bb
WizzzStark <wizzzstk@gmail.com>
2024-05-09 20:45:46 +0200

    Improved dashboard optimization

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -15,16 +15,18 @@ import { File, LayoutDashboard, Upload } from 'lucide-react';
 import FeaturedPrices from '../components/FeaturedPrices';
 import PersonalConsumption from '../components/PersonalConsumption';
 import { Toaster } from 'sonner';
+import { DragAndDrop } from '../components/DragAndDrop';
+import { FiDownload } from 'react-icons/fi';
 
 export default function Dashboard() {
     const [files, setFiles] = useState([]);
+    const [signal, setSignal] = useState(false);
     const [activeSidebarOption, setactiveSidebarOption] = useState(0);
     const [activeIndex, setActiveIndex] = useState(0);
 
     const [csvText, setCsvText] = useState('');
     const [data, setData] = useState([]);
     const [data2, setData2] = useState([]);
-    const [loading, setLoading] = useState(true);
 
     const [currentPrice, setCurrentPrice] = useState(null);
     const [minPrice, setMinPrice] = useState(null);
@@ -34,12 +36,13 @@ export default function Dashboard() {
     const [kilogramsCO2, setKilogramsCO2] = useState(0);
 
     useEffect(() => {
+
         const fetchData = async () => {
             const auth = getAuth();
             const user = auth.currentUser;
             const storage = getStorage();
 
-            if (user) {
+            if (user && files.length > 0) {
                 const userEmail = user.email.replace('.', ',');
                 const filePath = `uploads/${userEmail}/1.csv`;
                 const storageRef = ref(storage, filePath);
@@ -51,36 +54,27 @@ export default function Dashboard() {
                     parseCsv(text);
                 } catch (error) {
                     console.error('Error fetching file:', error);
-                    setLoading(false);
                 }
-            } else {
-                console.error('No user logged in');
-                setLoading(false);
             }
         };
 
-        fetchData();
-    }, []);
-
-
-    useEffect(() => {
         const auth = getAuth();
         onAuthStateChanged(auth, (user) => {
             if (user) {
-                console.log('User:', user);
                 const userEmail = user.email.replace('.', ',');
                 const userFilesRef = ref(getStorage(), `uploads/${userEmail}/`);
 
                 listAll(userFilesRef).then((result) => {
                     const fileNames = result.items.map(item => item.name);
                     setFiles(fileNames);
-                    console.log('Files2:', files);
+                    fetchData();
+
                 }).catch((error) => {
                     console.error('Error fetching files:', error);
                 });
             }
         });
-    }, []);
+    }, [signal, files.length]);
 
     useEffect(() => {
         const urls = {
@@ -122,8 +116,6 @@ export default function Dashboard() {
 
             setPercentageConsumption(percentageConsumption);
             setKilogramsCO2(totalCO2);
-            console.log('Consumo total:', totalConsumption);
-            console.log('CO2 total:', totalCO2);
         }
     }, [data])
 
@@ -156,7 +148,6 @@ export default function Dashboard() {
                 }));
                 setData(transformedData);
                 setCsvText(csvData);
-                setLoading(false);
             },
             skipEmptyLines: true,
             transform: (value, column) => {
@@ -172,7 +163,6 @@ export default function Dashboard() {
     };
 
     const changeActiveCSV = async (csvname) => {
-        console.log('Changing active CSV:', csvname);
         const auth = getAuth();
         const user = auth.currentUser;
         const storage = getStorage();
@@ -189,11 +179,9 @@ export default function Dashboard() {
                 parseCsv(text);
             } catch (error) {
                 console.error('Error fetching file:', error);
-                setLoading(false);
             }
         } else {
             console.error('No user logged in');
-            setLoading(false);
         }
     }
 
@@ -220,9 +208,7 @@ export default function Dashboard() {
         }, []);
     };
 
-    if (loading) {
-        return <div>Loading...</div>;
-    }
+
 
     const PieChartData = processData();
     const lightPriceData = processLightPriceData();
@@ -270,8 +256,8 @@ export default function Dashboard() {
         <div className='bg-black text-white flex'>
             <Toaster position="bottom-right" richColors />
 
-            <div className='sticky top-0 h-[100vh] '>
-                <SideBar activeSideBar={activeSidebarOption}>
+            <div className='sticky top-0 h-[100vh] z-[5]'>
+                <SideBar activeSideBar={activeSidebarOption} files={files}>
                     <SideBarElement icon={<LayoutDashboard size={20} />} text='Dashboard' active={activeSidebarOption === 0} onClick={() => handleSetActiveSideBar(0)} />
                     <SideBarElement icon={<Upload size={20} />} text='Upload File' active={activeSidebarOption === 1} onClick={() => handleSetActiveSideBar(1)} />
                     <hr className='my-3' />
@@ -291,51 +277,68 @@ export default function Dashboard() {
                 </SideBar>
             </div>
 
-            <section className='bg-black text-white flex-1 overflow-auto relative poppins backdrop-blur-md'>
-                <div className='relative z-[5]'>
 
-                    <FeaturedPrices currentPrice={currentPrice} minPrice={minPrice} maxPrice={maxPrice} />
+            {files.length > 0 ? (
+                <section className='bg-black text-white flex-1 overflow-auto relative poppins backdrop-blur-md'>
+                    <div className='relative z-[5]'>
 
-                    <div className='flex flex-col gap-10 mx-10 relative'>
-                        <DateAreaChart data={data2} text="Historic price MW/h in Spain" />
-                        <div className='flex flex-col md:flex-row gap-10 relative z-10'>
-                            <AreaChartComponent data={data} colors={COLORS} text="Light Consumption" yAxisValue="Consumo_KWh" />
-                            <AreaChartComponent data={data} colors={COLORS} text="Light Price" yAxisValue="Precio_KWh_euro" />
-                            <AreaChartComponent data={lightPriceData} colors={COLORS} text="Your light Price" yAxisValue="value" />
+                        <FeaturedPrices currentPrice={currentPrice} minPrice={minPrice} maxPrice={maxPrice} />
 
-                        </div>
-
-                    </div>
-
-                    <div className='flex flex-col md:flex-row gap-10 mx-10 relative mt-10'>
-                        <BarChartComponent data={co2Emissions} text="CO2 Consumption" yAxisValue="CO2" />
-                        <PieChartComponent data={PieChartData} activeIndex={activeIndex} onPieEnter={(e, index) => setActiveIndex(index)} colors={COLORS} />
-                    </div>
-
-                    <div className='flex flex-col md:flex-row gap-10 mx-10 relative mt-10'>
-                        <div className='flex flex-col justify-between border-[1px] p-5 bg-[#1E1E22]/50 border-[#E1E3F3]/5 rounded-md backdrop-blur-3xl'>
-                            <h2 className='text-[20px] font-semibold pb-1 pl-3 dashboard_mainText'>Awareness message</h2>
-                            <div className='flex w-full h-[200px]'>
+                        <div className='flex flex-col gap-10 mx-10 relative'>
+                            <DateAreaChart data={data2} text="Historic price MW/h in Spain" />
+                            <div className='flex flex-col md:flex-row gap-10 relative z-10'>
+                                <AreaChartComponent data={data} colors={COLORS} text="Light Consumption" yAxisValue="Consumo_KWh" />
+                                <AreaChartComponent data={data} colors={COLORS} text="Light Price" yAxisValue="Precio_KWh_euro" />
+                                <AreaChartComponent data={lightPriceData} colors={COLORS} text="Your light Price" yAxisValue="value" />
 
                             </div>
+
                         </div>
-                        <PersonalConsumption percentage={percentageConsumption} co2Consumed={kilogramsCO2} />
+
+                        <div className='flex flex-col md:flex-row gap-10 mx-10 relative mt-10'>
+                            <BarChartComponent data={co2Emissions} text="CO2 Consumption" yAxisValue="CO2" />
+                            <PieChartComponent data={PieChartData} activeIndex={activeIndex} onPieEnter={(e, index) => setActiveIndex(index)} colors={COLORS} />
+                        </div>
+
+                        <div className='flex flex-col md:flex-row gap-10 mx-10 relative mt-10 '>
+                            <PersonalConsumption percentage={percentageConsumption} co2Consumed={kilogramsCO2} />
+                        </div>
+
+                        <DownloadCSVButton csvData={csvText} />
+
                     </div>
+                    <div className='absolute z-[0] w-[30%] h-[20%] top-10 left-[460px] green__gradient opacity-70' />
+                    <div className='absolute z-[0] w-[20%] h-[25%] top-[100px] right-[10px] green__gradient opacity-50' />
+                    <div className='absolute z-[0] w-[10%] h-[20%] top-[600px] left-[10px] green__gradient opacity-80' />
+                    <div className='absolute z-[0] w-[20%] h-[20%] top-[390px] left-[700px] green__gradient opacity-45' />
+                    <div className='absolute z-[0] w-[20%] h-[20%] top-[700px] right-[70px] green__gradient opacity-45' />
+                    <div className='absolute z-[0] w-[25%] h-[25%] top-[900px] right-[200px] green__gradient opacity-40' />
+                    <div className='absolute z-[0] w-[30%] h-[35%] top-[1100px] left-[200px] green__gradient opacity-70' />
 
-                    <DownloadCSVButton csvData={csvText} />
+                </section>
+            ) : (
+                <>
+                    <div className='relative flex w-full flex-col h-[100vh] overflow-hidden'>
+                        <h1 className='text-[60px] poppins text-center mt-10 relative z-[5]'>Upload your first file</h1>
+                        <h2 className='text-[20px] poppins text-center relative z-[5]'>Start analyzing your electricity data</h2>
+                        <a className='relative bg-white w-[95%] mx-auto h-20 text-black hover:bg-gray-200 z-[5] flex justify-center items-center' href="/consumptions.csv">
+                            <div className='flex items-center gap-5'>
+                                <FiDownload size={20} className='text-black' />
+                                <p className='text-[16px] font-semibold'>
+                                    Download sample file
+                                </p>
+                            </div>
+                        </a>
+                        <DragAndDrop className="relative h-[75%] z-[5] w-[95%] mx-auto mt-10" setSignal={setSignal} />
+                        <div className='absolute z-[0] w-[20%] h-[40%] top-10 left-[460px] green__gradient opacity-30' />
+                        <div className='absolute z-[0] w-[20%] h-[40%] top-80 left-[700px] green__gradient opacity-30' />
+                        <div className='absolute z-[0] w-[20%] h-[45%] top-[100px] right-[10px] green__gradient opacity-30' />
+                        <div className='absolute z-[0] w-[30%] h-[20%] top-[600px] left-[10px] green__gradient opacity-40' />
+                        <div className='absolute z-[0] w-[20%] h-[50%] top-[700px] right-[70px] green__gradient opacity-35' />
+                    </div>
+                </>
+            )}
 
-                </div>
-
-
-                <div className='absolute z-[0] w-[30%] h-[20%] top-10 left-[460px] green__gradient opacity-70' />
-                <div className='absolute z-[0] w-[20%] h-[25%] top-[100px] right-[10px] green__gradient opacity-50' />
-                <div className='absolute z-[0] w-[10%] h-[20%] top-[600px] left-[10px] green__gradient opacity-80' />
-                <div className='absolute z-[0] w-[20%] h-[20%] top-[390px] left-[700px] green__gradient opacity-45' />
-                <div className='absolute z-[0] w-[20%] h-[20%] top-[700px] right-[70px] green__gradient opacity-45' />
-                <div className='absolute z-[0] w-[25%] h-[25%] top-[900px] right-[200px] green__gradient opacity-40' />
-                <div className='absolute z-[0] w-[30%] h-[35%] top-[1100px] left-[200px] green__gradient opacity-70' />
-
-            </section>
         </div>
     );
 }
