f20edd7594328f3580a47e9fa2815c3df5a8a1e5
WizzzStark <wizzzstk@gmail.com>
2024-05-09 20:54:14 +0200

    config key change

--- firebase-config.js
+++ firebase-config.js
@@ -3,13 +3,13 @@ import { getStorage } from "firebase/storage";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyAEZoqdSvfAFoIEdY7RASAI2RNWrzZfaaY",
-    authDomain: "wattcheck-52a95.firebaseapp.com",
-    projectId: "wattcheck-52a95",
-    storageBucket: "wattcheck-52a95.appspot.com",
-    messagingSenderId: "559272381271",
-    appId: "1:559272381271:web:2cb206e3125ae7ae4e0c12"
-  };
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID
+};
 
 const app = initializeApp(firebaseConfig);
 const storage = getStorage(app);
